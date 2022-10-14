ab19bc0ce7843bb0714821d647d09559f856004f
W3IRDE3K0 <jddllv@hotmail.com>
2022-10-13 15:00:20 -1000

    Update

--- passwdgen.py
+++
@@ -1,8 +0,0 @@
-            def generatePassword():
-                var result           = ''
-                var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()'
-                var charactersLength = characters.length
-                for  var i = 0; i < 16; i++ :
-                  result += characters.charAt(Math.floor(Math.random() * charactersLength))
-
-               return result;
